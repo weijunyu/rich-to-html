19c65093419cf1fd9e94512b8f16cdc0553a5124
Wei Junyu <weijunyu@users.noreply.github.com>
2020-01-20 17:28:48 +0800

    Basic editor functions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,262 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app sgds-container">
+    <div class="editor content has-default-header-styles">
+      <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+        <div class="menubar">
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.bold() }"
+            @click="commands.bold"
+          >
+            <i class="fas fa-bold"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.italic() }"
+            @click="commands.italic"
+          >
+            <i class="fas fa-italic"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.strike() }"
+            @click="commands.strike"
+          >
+            <i class="fas fa-strikethrough"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.underline() }"
+            @click="commands.underline"
+          >
+            <i class="fas fa-underline"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.code() }"
+            @click="commands.code"
+          >
+            <i class="fas fa-code"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.paragraph() }"
+            @click="commands.paragraph"
+          >
+            <i class="fas fa-paragraph"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+            @click="commands.heading({ level: 1 })"
+          >
+            H1
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+            @click="commands.heading({ level: 2 })"
+          >
+            H2
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+            @click="commands.heading({ level: 3 })"
+          >
+            H3
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.bullet_list() }"
+            @click="commands.bullet_list"
+          >
+            <i class="fas fa-list-ul"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.ordered_list() }"
+            @click="commands.ordered_list"
+          >
+            <i class="fas fa-list-ol"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.blockquote() }"
+            @click="commands.blockquote"
+          >
+            <i class="fas fa-quote-right"></i>
+          </button>
+
+          <button
+            class="menubar__button"
+            :class="{ 'is-active': isActive.code_block() }"
+            @click="commands.code_block"
+          >
+           <i class="fas fa-code"></i>(block)
+          </button>
+
+          <button class="menubar__button" @click="commands.horizontal_rule">
+            --
+          </button>
+
+          <button class="menubar__button" @click="commands.undo">
+            <i class="fas fa-undo"></i>
+          </button>
+
+          <button class="menubar__button" @click="commands.redo">
+            <i class="fas fa-redo"></i>
+          </button>
+        </div>
+      </editor-menu-bar>
+
+      <editor-content class="editor__content" :editor="editor" />
+    </div>
+
+    <div class="actions">
+      <button class="button" @click="clearContent">
+        Clear Content
+      </button>
+      <button class="button" @click="setContent">
+        Set Content
+      </button>
+    </div>
+
+    <div class="export">
+      <h3>JSON</h3>
+      <pre><code v-html="json"></code></pre>
+
+      <h3>HTML</h3>
+      <pre><code>{{ beautify(html) }}</code></pre>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Editor, EditorContent, EditorMenuBar } from "tiptap";
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History
+} from "tiptap-extensions";
+import prettier from "prettier/standalone";
+import parserHtml from "prettier/parser-html";
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    EditorContent,
+    EditorMenuBar
+  },
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History()
+        ],
+        content: `
+          <h2>
+            Export HTML or JSON
+          </h2>
+          <p>
+            You are able to export your data as <code>HTML</code> or <code>JSON</code>.
+          </p>
+        `,
+        onUpdate: ({ getJSON, getHTML }) => {
+          this.json = getJSON();
+          this.html = getHTML();
+        }
+      }),
+      json: "Update content to see changes",
+      html: "Update content to see changes"
+    };
+  },
+  methods: {
+    beautify(html) {
+      return prettier.format(html, {
+        parser: "html",
+        plugins: [parserHtml]
+      });
+    },
+    clearContent() {
+      this.editor.clearContent(true);
+      this.editor.focus();
+    },
+    setContent() {
+      // you can pass a json document
+      this.editor.setContent(
+        {
+          type: "doc",
+          content: [
+            {
+              type: "paragraph",
+              content: [
+                {
+                  type: "text",
+                  text: "This is some inserted text. 👋"
+                }
+              ]
+            }
+          ]
+        },
+        true
+      );
+      // HTML string is also supported
+      // this.editor.setContent('<p>This is some inserted text. 👋</p>')
+      this.editor.focus();
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.menubar {
+  display: flex;
+  margin: .5rem 0;
+}
+.menubar .menubar__button {
+  flex: 1 1 auto;
+}
+.editor__content {
+  border: 1px solid black;
 }
 </style>
